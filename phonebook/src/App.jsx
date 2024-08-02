59e630c7621716d8bf5e02c1b2031e7cf81fa4b3
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-08-02 20:04:01 +0300

    Complete exercises 2.6 and 2.7

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!newName) return;
+    const isDuplicate = persons.some((person) => person.name === newName);
+
+    if (isDuplicate) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    const newPersonObj = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPersonObj));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
